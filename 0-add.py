9a96b25b6f830aa88d42c9b7c93b463f4bbb142f
dawit <dawitxxd@gmail.com>
2024-08-01 11:38:26 +0300

    anotation basics

---
+++ 0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Type Anotation
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    A type-annotated function that takes float values
+    a and b as arguments and returns their sum as a float
+    """
+    return (a + b)
