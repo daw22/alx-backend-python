f8e7ff971589478de613b11f009b6b4e69d987e0
dawit <dawitxxd@gmail.com>
2024-08-26 13:43:18 +0300

    Parameterize a patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,7 @@
 """
 Parametrized unit test
 """
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 import unittest
 import requests
 from parameterized import parameterized
@@ -49,6 +49,33 @@ class TestGetJson(unittest.TestCase):
         result = get_json(test_url)
         self.assertEqual(result, test_payload)
         mock_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    Tests a function with @memoize decorator
+    """
+    def test_memoize(self):
+        """
+        checks if a memoized function is called only
+        once for similar args(no args this case)
+        """
+        class TestClass:
+            """
+            test class
+            """
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock_func:
+            tc = TestClass()
+            tc.a_property()
+            tc.a_property()
+            mock_func.assert_called_once()
 
 
 if __name__ == "__main__":
